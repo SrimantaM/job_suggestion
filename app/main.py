e1b5ceb72af8f2270ce41a98df76a1812b6cee63
Srimanta Mondal <mondalayanbrothe.srimanta7@gmail.com>
2026-04-16 19:19:27 +0530

    Primitive structure is defined. Functionalities need to be implemented

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
-from fastapi.requests import Request
-from fastapi.staticfiles import StaticFiles
+# from fastapi.responses import RedirectResponse
+# from fastapi.requests import Request
+# from fastapi.staticfiles import StaticFiles
 # from fastapi.security import OAuth2PasswordBearer, SecurityScopes
 # from fastapi.security.utils import get_authorization_scheme
-from fastapi.openapi.docs import get_swagger_ui_html
+# from fastapi.openapi.docs import get_swagger_ui_html
 # from fastapi.openapi.routers import get_openapi
 # from fastapi.openapi.security import get_security_scheme
 # from fastapi.openapi.security.oauth2 import get_bearer_jsw_token_credentials
-from fastapi.security.oauth2 import SecurityScopes
-from starlette.requests import Request
-from starlette.responses import JSONResponse, RedirectResponse
-from starlette.status import HTTP_302_FOUND
-from starlette.types import Receive, Scope, Send
-from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.security.oauth2 import SecurityScopes
+# from starlette.requests import Request
+# from starlette.responses import JSONResponse, RedirectResponse
+# from starlette.status import HTTP_302_FOUND
+# from starlette.types import Receive, Scope, Send
+# from fastapi.middleware.cors import CORSMiddleware
 
 
 from auth import login, registration
@@ -26,17 +26,17 @@ app = FastAPI(
     
 )
 
-app.include_router(login.router)
-app.include_router(registration.router)
+# app.include_router(login.router)
+# app.include_router(registration.router)
 app.include_router(dashboard.router)
 app.include_router(healthcheck.router)
 
 # app.mount("/static", StaticFiles(directory="static"), name="static")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
